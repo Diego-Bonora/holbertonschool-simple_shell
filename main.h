11f203eed70af90f9580bc28cb434b436217daee
Diego Bonora <bonoradi@gmail.com>
2023-04-19 07:24:27 -0700

    structure added for tokens

--- main.h
+++ main.h
@@ -8,7 +8,25 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * struct token_s - linked list
+ * @token: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct token_s
+{
+	char *token;
+	unsigned int len;
+	struct token_s *next;
+} token_t;
+
+token_t *add_node_end(token_t **head, const char *str);
 char **split(char *line);
 int execute(char **args);
+char **convert(token_t **args);
 
 #endif
