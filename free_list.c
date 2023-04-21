002006b755e0d8c5eae6785d8754a67f9a4fd5ce
Diego-Bonora <bonoradi@gmail.com>
2023-04-21 09:46:21 -0700

    Function that frees a linked list

---
+++ free_list.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: input linked list
+ * Return: Nothing
+ */
+
+void free_list(token_t *head)
+{
+	token_t *temp;
+
+	temp = head;
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp->token);
+		free(temp);
+		temp = head;
+	}
+}
