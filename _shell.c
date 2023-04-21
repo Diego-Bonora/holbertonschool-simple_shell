c2695fdc669aa0d6ff996d4412bb6a2ca22deadf
Diego-Bonora <bonoradi@gmail.com>
2023-04-21 13:21:34 -0700

    Main function for calling all the other functions

--- _shell.c
+++ _shell.c
@@ -4,27 +4,50 @@
 
 int main(void)
 {
-    int count = 0, status = 1;
-    token_t *test, *path_token;
+    int count, status = 1, free_ = 0;
+    ssize_t _exit_ = 0;
+    token_t *args_token, *path_token;
     char *line = NULL, *path = NULL, **array = NULL;
 
     while (status)
     {
-        line = Read_line();
-        test = tokenicer(line, " \t\r\n\a");
+        count = 0;
+        line = Read_line(&_exit_);
+        if (_exit_ == -1)
+            return (0);
+        args_token = tokenicer(line, " \t\r\n\a");
         path = _getenv("PATH");
         path_token = tokenicer(path, ":");
-        test = _stat_checker(test, path_token);
-        free(path_token);
+        args_token = _stat_checker(args_token, path_token);
+        array = _list_to_array(args_token);
+        if (strcmp(array[0], "exit") == 0 && array[1] == NULL)
+        {
+            free(array[0]);
+            free(array);
+            free(path);
+            free(line);
+            return (0);
+        }
+        status = _exeCute(array);
+        while(array[free_])
+        {
+            free(array[free_]);
+            free_++;
+        }
+        free(array);
+        free(path);
+        free(line);
     }
+    return (0);
 }
 
-char *Read_line(void)
+char *Read_line(ssize_t *_exit_)
 {
     char *str = NULL;
     size_t size;
-    
+
+    printf("DEPS -> ");
     str = malloc(sizeof(char) * 64);
-    getline(&str, &size, stdin);
+    *_exit_ = getline(&str, &size, stdin);
     return(str);
 }
