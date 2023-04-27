0e2cec85b4d4b5b7657bb1b6981283bad646b542
Diego-Bonora <bonoradi@gmail.com>
2023-04-27 06:36:33 -0700

    modified the Read_line function for reading and exiting all in the same function

--- _shell.c
+++ _shell.c
@@ -7,12 +7,13 @@
  * Return: 0 on success and stat on exit
 */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {	int count = 1, status = 1, free_;
 	ssize_t _exit_ = 0;
 	token_t *args_token = NULL, *path_token = NULL, *command = NULL;
 	char *line = NULL, *path = NULL, **array = NULL;
 
+	(void)free_;
 	while (status)
 	{	free_ = 0;
 		line = Read_line(&_exit_);
@@ -20,34 +21,24 @@ int main(__attribute__((unused)) int argc, char *argv[])
 		if (!args_token)
 		{	free(line);
 			continue; }
-		if (strcmp(args_token->token, "exit") == 0 && args_token->next == NULL)
-		{	free_list(args_token);
-			free(line);
-			if (_exit_ == 0)
-				exit(0);
-			exit(127); }
 		path = _getenv("PATH");
 		path_token = tokenicer(path, ":");
 		if (!path_token)
 		{	free_list(args_token);
 			free(line);
 			continue; }
-		command = _stat_checker(args_token, path_token);
+		command = _stat_checker(args_token, path_token, line, count);
 		if (!command)
-		{	fprintf(stderr, "%s: %d: %s: not Found\n", argv[0], count, line);
-			free(line);
-			_exit_ = 1;
+		{	_exit_ = 1;
 			count++;
+			puts("aa");
 			continue; }
 		else
 			_exit_ = 0;
+		puts("a");
 		array = _list_to_array(args_token);
 		status = _exeCute(array);
-		while (array[free_])
-		{	free(array[free_]);
-			free_++; }
 		count++;
-		free(array);
 		free(line); }
 	return (0); }
 
@@ -66,8 +57,15 @@ char *Read_line(ssize_t *_exit_)
 
 	if (fd == 1)
 		printf("DEPS -> ");
-	num = getline(&str, &size, stdin);
-	if (num == -1)
+	
+	if (getline(&str, &size, stdin) == -1)
+	{
+		free(str);
+		if (*_exit_ == 0)
+			exit(0);
+		exit(127);
+	}
+	else if (strcmp(str, "exit\n") == 0)
 	{
 		free(str);
 		if (*_exit_ == 0)
